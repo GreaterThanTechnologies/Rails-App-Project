02efa981ea70648c4b6fa2def1f3f88333091d6a
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-03-30 17:47:43 -0400

    adds client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,10 @@
 class Client < ApplicationRecord
+
+  belongs_to :manager 
+  belongs_to :project
+
+  has_secure_password
+
+  validates_presence_of :username, :name, :email
+  validates_uniqueness_of :username
 end
